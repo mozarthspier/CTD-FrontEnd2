4a13c265799822f386237cf4b359c3f8e350219e
Mozarth Spier <mozarthspier@gmail.com>
2022-03-23 22:12:39 -0300

    Adiciona permanência no localStorage

--- aula-12/main.js
+++ aula-12/main.js
@@ -1,13 +1,21 @@
 let inputReference = document.querySelector("input");
 let buttonReference = document.querySelector("button");
 let sectionReference = document.querySelector("section");
-let posts = [];
+
+let posts;
+if (localStorage.getItem("posts")) {
+    posts = JSON.parse(localStorage.getItem("posts"));
+    renderizaPosts(posts);
+} else {
+    posts = [];
+}
 
 buttonReference.addEventListener("click", event => {
     
     event.preventDefault();
     posts.unshift(inputReference.value);
 
+    localStorage.setItem("posts", JSON.stringify(posts));
     renderizaPosts(posts);
 
 })
